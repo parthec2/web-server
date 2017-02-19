4f238f49589eeda977129854e2f67a923d99caca
parth shah ec2 <parth.ec2@gmail.com>
2017-02-19 16:25:10 +0530

    Add help route

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About Us!');
 });
 
+app.get('/help',function(req,res){
+	res.send("Welcome to Help page");
+});
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function () {
